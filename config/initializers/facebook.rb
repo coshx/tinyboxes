2e23bc0a4d4b0ca60e602ad4b14c0e6a19675dc3
Ben Taitelbaum <ben@coshx.com>
2011-10-15 18:21:10 -0400

    Switches prototype login.rjs to non-prototype

--- config/initializers/facebook.rb
+++ config/initializers/facebook.rb
@@ -1,5 +1,5 @@
-FB_APP_ID = ENV['FB_APP_ID']
-FB_APP_SECRET = ENV['FB_APP_SECRET']
-FB_PAGE_NAME = ENV['FB_PAGE_NAME']
+FB_APP_ID = ENV['FB_APP_ID'] || "241186319261059"
+FB_APP_SECRET = ENV['FB_APP_SECRET'] || "8c0ad7c56b53fef4bfd2337edc23a7bb"
+FB_PAGE_NAME = ENV['FB_PAGE_NAME'] || "TinyBoxes-demo"
 
 FB_PERMS = 'email, publish_stream, offline_access'
